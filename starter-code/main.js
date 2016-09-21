f4e2f53fba7339647ff165397deaf196e45ce339
Ari Crosby <REXB@Aris-MBP.lan>
2016-09-21 12:09:31 +1000

    added to game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,15 @@
-//created card logic//
+
 var cardOne ='queen';
 var cardTwo = 'queen';
 var cardThree ='king';
 var cardFour ='king';
+
+if ('queen' === 'queen') { 
+	alert("You found a match!");
+}
+else if ('king' === 'king') { 
+	alert("You found a match!");
+}
+else ('queen' === 'king'); { 
+	alert("Sorry, try again.");
+}
